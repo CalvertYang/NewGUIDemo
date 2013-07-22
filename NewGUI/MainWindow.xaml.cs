6b34f48ac058111f5f17df742ca2325dc503c5f6
AndyYou <andyyu0920@gmail.com>
2013-07-22 15:20:46 +0800

    update database to mdb

--- NewGUI/MainWindow.xaml.cs
+++ NewGUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace NewGUI
 {
@@ -22,6 +25,29 @@ namespace NewGUI
         public MainWindow()
         {
             InitializeComponent();
+           
+
+           
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string connstring = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=webdb.mdb";
+            string query = "Select TOP 1 * From Jobs";
+            WebDBDataSet ds = new WebDBDataSet();
+
+            using (OleDbConnection conn = new OleDbConnection(connstring))
+            {
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(ds.Jobs);
+                foreach (var i in ds.Jobs)
+                {
+                    lbTest.Content = i.Operator;
+                }
+
+            }
         }
     }
 }
